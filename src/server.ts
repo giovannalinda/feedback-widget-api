dbfda38fd74ede71184fa127480ae44222554122
Giovanna Linda <eugiovannasouza@gmail.com>
2022-05-15 10:39:43 -0300

    feedback route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
 import express from 'express'
+import { prisma } from './prisma'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-  return response.send('hello world')
+// GET = Buscar informações
+// POST = Cadastrar informações
+// PUT = Atualizar informações de uma entidade
+// PATCH = Atualizar uma informação única de uma entidade
+// DELETE = Deletar uma informação
+
+app.use(express.json())
+
+app.post('/feedbacks', async (request, response) => {
+  const { type, comment, screenshot } = request.body
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  return response.status(201).json(feedback)
 })
 
 app.listen(3333, () => {
