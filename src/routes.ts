57576b8c30f308b526ad5ba19705d5ce0ec5a739
Giovanna Linda <71105672+giovannalinda@users.noreply.github.com>
2022-05-15 15:46:56 -0300

    feedback route (#1)

---
+++ src/routes.ts
@@ -0,0 +1,26 @@
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter'
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository'
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case'
+import express from 'express'
+
+export const routes = express.Router()
+
+routes.post('/feedbacks', async (request, response) => {
+  const { type, comment, screenshot } = request.body
+
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+  const nodemailMailAdapter = new NodemailerMailAdapter()
+
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository,
+    nodemailMailAdapter
+  )
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
+  })
+
+  return response.status(201).send()
+})
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
+import { routes } from './routes'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-  return response.send('hello world')
-})
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('🔥 Server started at http://localhost:3333')
