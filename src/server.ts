5ca7d80e3bd073ffaea731535a4426871b3941dc
Giovanna Linda <eugiovannasouza@gmail.com>
2022-05-15 09:17:59 -0300

    config prisma

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express()
 
+app.get('/users', (request, response) => {
+  return response.send('hello world')
+})
+
 app.listen(3333, () => {
   console.log('🔥 Server started at http://localhost:3333')
 })
